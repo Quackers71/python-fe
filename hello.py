bc34d1c39aba53c13c2f99ba1a705a475483ba2f
Quackers <robq71@gmail.com>
2022-12-11 21:14:01 +0000

    completed Chapter 1

---
+++ hello.py
@@ -0,0 +1,21 @@
+print("Hello World!")
+x = 2
+print("x =",x)
+x += 2
+print("x + x =",x)
+
+# Conditional Steps
+y = 5
+print("y =",y)
+if y < 10:
+    print("y < 10")
+if y > 20:
+    print("y !> 20")
+print("Finish")
+
+# Repeated Steps
+n = 5
+while n > 0 :
+    print(n)
+    n = n - 1  # or use n -= 1
+print("Blastoff!")
