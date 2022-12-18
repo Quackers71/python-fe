32570c53a8204847fda1774853ea82528331547d
Quackers <robq71@gmail.com>
2022-12-18 18:37:44 +0000

    placed try/catch in a func

--- multi_nums.py
+++ multi_nums.py
@@ -1,33 +1,34 @@
+from colorama import Fore, Style
+
 def addtwo(a, b):
     added = a + b
     return added
 
-x = input("Enter 1st number: ")
-try:
-    inp1 = float(x)
-except:
-    inp1 = -1
-    x = 0
+def trycatch(n):
+    try:
+        inp = float(n)
+    except:
+        inp = -1
 
-if inp1 > 0:
-    print("Ok, thanks")
-else:
-    print("not a number!")
+    if inp > 0:
+        print("Ok, thanks")
+    else:
+        print(Fore.RED + "not a number!" + Style.RESET_ALL)
 
-y = input("Enter 2nd number: ")
-try:
-    inp2 = float(y)
-except:
-    inp2 = -1
-    y = 0
+v = input("Enter 1st number: ")
+trycatch(v)
 
-if inp2 > 0:
-    print("Ok, thanks")
-else:
-    print("not a number!")
+w = input("Enter 2nd number: ")
+trycatch(w)
 
-z = addtwo(int(x), int(y))
-if float(z) != 0:
+x = input("Enter 3rd number: ")
+trycatch(x)
+
+y = input("Enter 4th number: ")
+trycatch(y)
+
+if v.isnumeric() and w.isnumeric() and x.isnumeric() and y.isnumeric():
+    z = addtwo(int(x), int(y))
     print("The total is",z)
 else:
     print("There was no total...")
