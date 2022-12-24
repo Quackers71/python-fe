387fc5d255d5070332b65755daad1c096ab7cc9e
Quackers <robq71@gmail.com>
2022-12-24 17:06:17 +0000

    fixed extract email

--- Files/extract_email_from_file.py
+++ Files/extract_email_from_file.py
@@ -1,10 +1,8 @@
 fhand = open("Files/mbox-short.txt")
 for line in fhand:
     line = line.rstrip()
-    if "media" in line:
+    if "edu" in line:
         sppos0 = line.find(" ")
         atpos = line.find("@")
-        sppos1 = line.find(" ",atpos)
-        addr = line[sppos0+1 : sppos1]
         if line.startswith("From: "):
-            print(addr)
+            print(line[6:])
